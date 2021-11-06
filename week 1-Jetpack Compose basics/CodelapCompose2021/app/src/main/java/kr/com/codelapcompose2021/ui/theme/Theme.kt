e4a9c9ec72e6d5ee3d43e4bc031131b12dbc12f5
bean628 <dudwp628@naver.com>
2021-11-06 12:06:31 +0900

    [ComposeFest2021 코드랩 1주차] Jetpack Compose basis 12 Styling and theming your app 완료

--- week 1-Jetpack Compose basics/CodelapCompose2021/app/src/main/java/kr/com/codelapcompose2021/ui/theme/Theme.kt
+++ week 1-Jetpack Compose basics/CodelapCompose2021/app/src/main/java/kr/com/codelapcompose2021/ui/theme/Theme.kt
@@ -5,27 +5,45 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 
-private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
-)
+//private val DarkColorPalette = darkColors(
+//    primary = Purple200,
+//    primaryVariant = Purple700,
+//    secondary = Teal200
+//)
+
+//private val LightColorPalette = lightColors(
+//    primary = Purple500,
+//    primaryVariant = Purple700,
+//    secondary = Teal200
+//
+//    /* Other default colors to override
+//    background = Color.White,
+//    surface = Color.White,
+//    onPrimary = Color.White,
+//    onSecondary = Color.Black,
+//    onBackground = Color.Black,
+//    onSurface = Color.Black,
+//    */
+//)
+
 
 private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200
-
-    /* Other default colors to override
-    background = Color.White,
-    surface = Color.White,
-    onPrimary = Color.White,
-    onSecondary = Color.Black,
-    onBackground = Color.Black,
-    onSurface = Color.Black,
-    */
+    surface = Blue,
+    onSurface = Color.White,
+    primary = LightBlue,
+    onPrimary = Navy
 )
+
+private val DarkColorPalette = darkColors(
+    surface = Blue,
+    onSurface = Navy,
+    primary = Navy,
+    onPrimary = Chartreuse
+)
+
+
 
 @Composable
 fun CodelapCompose2021Theme(
