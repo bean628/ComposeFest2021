592a9be85df54a5d0bdee9569506ec4feef16c33
bean628 <dudwp628@naver.com>
2021-11-21 14:55:29 +0900

    [ComposeFest2021 코드랩 2-1 주차] Layouts In Jetpack Compose Material component

---
+++ week 2-1-Layouts in JetpackCompose/LayoutsInJetpackCompose/app/src/main/java/kr/com/layoutsinjetpackcompose/LayoutCodelab.kt
@@ -0,0 +1,56 @@
+package kr.com.layoutsinjetpackcompose
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import kr.com.layoutsinjetpackcompose.ui.theme.LayoutsInJetpackComposeTheme
+
+
+/*
+   Scaffold를 사용하면 기본 Material Design 레이아웃 구조로 UI를 구현할 수 있습니다.
+   TopAppBar, BottomAppBar, FloatingActionButton 및 Drawer와 같은 가장 일반적인 최상위 Material
+   구성 요소에 대한 슬롯을 제공합니다.
+   Scaffold를 사용하면 이러한 구성 요소가 올바르게 배치되고 함께 작동하는지 확인합니다.
+ */
+
+@Preview
+@Composable
+fun LayoutsCodelabPreview() {
+    LayoutsInJetpackComposeTheme {
+        LayoutsCodelab()
+    }
+}
+
+@Composable
+fun LayoutsCodelab() {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(text = "LayoutsCodelab")
+                },
+                actions = {
+                    IconButton(onClick = { /* doSomething() */ }) {
+                        Icon(Icons.Filled.Favorite, contentDescription = null)
+                    }
+                }
+            )
+        }
+    ) { innerPadding ->
+        BodyContent(Modifier.padding(innerPadding))
+    }
+}
+
+@Composable
+fun BodyContent(modifier: Modifier = Modifier) {
+    Column(modifier = modifier.padding(8.dp)) {
+        Text(text = "Hi there!")
+        Text(text = "Thanks for going through the Layouts codelab")
+    }
+}
