631f2a46f267368867beb39f73f554a9dd5a21f3
bean628 <dudwp628@naver.com>
2021-11-29 16:34:15 +0900

    [ComposeFest2021 코드랩 3-1 주차] Jetpack Compose theming - Material Theming, Define your theme
    새로운 Theme 를 만들어서 적용하는 걸 배움. 폰트 패밀리, 컬러, 쉐이프 바꾸는 방법 학습

---
+++ week 3-1-Jetpack Compose theming/app/src/main/java/com/codelab/theming/ui/start/theme/Color.kt
@@ -0,0 +1,14 @@
+package com.codelab.theming.ui.start.theme
+
+import androidx.compose.ui.graphics.Color
+
+
+// (주의) Color 패키지는 androidx.compose.ui.graphics.Color 입니다.
+val Red700 = Color(0xffdd0d3c)
+val Red800 = Color(0xffd00036)
+val Red900 = Color(0xffc20029)
+
+
+// For Dark Theme
+val Red200 = Color(0xfff297a2)
+val Red300 = Color(0xffea6d7e)
